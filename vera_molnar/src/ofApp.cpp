4d7097c7723085263fc395787493ce597cdb4a15
Ilona Brand <theilonabrand@gmail.com>
2018-09-23 20:50:39 -0400

    Mechanic #1: Set up a 9x6 grid of white rectangles on an orange background
    Also added Vera's Molnar's source image to the repo for reference

--- vera_molnar/src/ofApp.cpp
+++ vera_molnar/src/ofApp.cpp
@@ -2,7 +2,8 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofBackground(202, 119, 93);
+    ofColor(255);
 }
 
 //--------------------------------------------------------------
@@ -10,9 +11,36 @@ void ofApp::update(){
 
 }
 
+void ofApp::drawRect(int x, int y, int w, int h){
+    ofPolyline poly;
+    
+    poly.addVertex(x, y);
+    poly.addVertex(x + w, y);
+    poly.addVertex(x + w, y + h);
+    poly.addVertex(x, y + h);
+    
+    poly.close();
+    poly.draw();
+}
+
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    int rectWidth = 70;
+    int spaceBetweenRect = 10;
+    
+    ofTranslate(150, 150);
+    
+    for (int i = 0; i < 6; i++) {
+        ofPushMatrix();
+            ofTranslate(0, i * (rectWidth + spaceBetweenRect));
+            for (int j = 0; j < 9; j++) {
+                ofPushMatrix();
+                    ofTranslate(j * (rectWidth + spaceBetweenRect), 0);
+                    drawRect(0, 0, rectWidth, rectWidth);
+                ofPopMatrix();
+            }
+        ofPopMatrix();
+    }
 }
 
 //--------------------------------------------------------------
